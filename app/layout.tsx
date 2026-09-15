f50e928054a05d33a68d54552af061dbc4f0f144
nederlanddata <nederlanddata2026@gmail.com>
2026-09-15 12:27:59 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Navbar />
         <main className="max-w-5xl mx-auto px-6 py-8">{children}</main>
         <Footer />
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="75300f01-037e-410a-b197-c4d82f73f768"></script>
       </body>
     </html>
   );
